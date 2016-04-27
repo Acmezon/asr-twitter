d9586bb8032656fde792d5854d1556ac52239f91
Ale Sánchez <alesanchezmedina@gmail.com>
2016-04-27 12:08:36 +0200

    Preprocesamiento y carga

---
+++ corpus/load_sanders_tweets.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, '../preprocessing')
+
+import csv
+import json
+import os
+
+from tweet import sanitize
+
+
+with open('sanders/corpus.csv', 'r') as corpus:
+    reader = csv.reader(corpus, delimiter=',', quotechar='"')
+    
+    with open('sanders/tweets.txt', 'w') as tweets:
+        with open('sanders/classification.txt', 'w') as classification:
+            for row in reader:
+                if row[1] == 'irrelevant':
+                    continue
+                filename = row[-1]
+                if os.path.isfile('sanders/rawdata/{0}.json'.format(filename)):
+                    with open('sanders/rawdata/{0}.json'.format(filename), 'r') as tweet_file:
+                        tweet = json.load(tweet_file)
+                        print(tweet['text'].replace('\n', ' '), file=tweets)
+                        print(row[1], file=classification)
+                        
+tweets = []
+with open('sanders/tweets.txt', 'r') as file_tweets:
+    for line in file_tweets:
+        tweets.append(sanitize(line))
+
+with open('sanders/tweets.txt', 'w') as file_tweets:
+    for tweet in tweets:
+        file_tweets.write(tweet + "\n")
