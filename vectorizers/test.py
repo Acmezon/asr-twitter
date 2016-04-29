36e4c11de43a88fafa4a84f2d98a2f4cbcf0a50d
dandelea <dandelea@alum.us.es>
2016-04-29 10:22:39 +0200

    Vectorizer2 finalizado

--- vectorizers/test.py
+++ vectorizers/test.py
@@ -3,6 +3,9 @@ from vectorizer2 import Vectorizer2
 v = Vectorizer2(stop_words='english')
 print(v)
 
-documentos = ["This is text 1", "This is other hell of a bad text", "This is the best awesome text of life"]
+with open('data/tweets.txt') as f:
+    documentos = f.readlines()
+
+#documentos = ["This is text 1", "This is other hell of a bad text", "This is the best awesome text of life"]
 aux = v.fit_transform(documentos)
 print(aux)
--- vectorizers/vectorizer2.py
+++ vectorizers/vectorizer2.py
@@ -2,10 +2,9 @@ import nltk
 import numpy as np
 
 from collections import Counter
-
+from scipy.sparse import csc_matrix
 from sklearn.base import BaseEstimator
 from sklearn.feature_extraction.text import VectorizerMixin
-
 from nltk.corpus import sentiwordnet as swn
 
 class Vectorizer2(BaseEstimator, VectorizerMixin):
@@ -38,24 +37,47 @@ class Vectorizer2(BaseEstimator, VectorizerMixin):
         else:
             self.tokenizer = nltk.word_tokenize
         self.preprocessor = self.build_preprocessor()
-        self.part_of_speech = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRT', 'PRON', 'VERB', '.', 'X']
+        self.part_of_speech = self.get_feature_names()[3:]
+        self.sentiments = {}
 
     def _remove_stop_words(self, text):
+        """
+        Deletes all the stop_words previously defined from a text
+        """
         tokens = self.build_tokenizer()(text)
         stops = self.get_stop_words()
         result = [x for x in tokens if x not in stops]
         return " ".join(result)
 
     def _get_sentiment(self, word):
-        synsets = list(swn.senti_synsets(word.lower()))
-        pos_score = sum(ss.pos_score() for ss in synsets)/float(len(synsets))
-        neg_score = sum(ss.neg_score() for ss in synsets)/float(len(synsets))
-        obj_score = sum(ss.obj_score() for ss in synsets)/float(len(synsets))
-        return {'pos_score': pos_score,
-                'neg_score': neg_score,
-                'obj_score': obj_score}
+        """
+        Retrieve the sentiments of a word via SentiWordNet lexicon.
+        If not previously learned, calculate itself.
+        It is calculated as the average sentiment of all its synsets.
+        """
+        if word in self.sentiments:
+            return self.sentiments[word]
+        else:
+            synsets = list(swn.senti_synsets(word.lower()))
+            if len(synsets)>0:
+                pos_score = sum(ss.pos_score() for ss in synsets)/float(len(synsets))
+                neg_score = sum(ss.neg_score() for ss in synsets)/float(len(synsets))
+                obj_score = sum(ss.obj_score() for ss in synsets)/float(len(synsets))
+                sentiment = {'pos_score': pos_score,
+                        'neg_score': neg_score,
+                        'obj_score': obj_score}
+            else:
+                sentiment = {'pos_score': 0.0,
+                        'neg_score': 0.0,
+                        'obj_score': 1.0}
+            self.sentiments[word] = sentiment
+        return sentiment
 
     def _get_avg_sentiment(self, text):
+        """
+        Calculate the sentiments of a text as the avergage sentiments of its words.
+        The sentiment values are retrieved via SentiWordNet lexicon.
+        """
         words = self.build_tokenizer()(text)
         sentiments = [self._get_sentiment(word) for word in words]
         pos_score = sum(s['pos_score'] for s in sentiments)/float(len(sentiments))
@@ -66,6 +88,14 @@ class Vectorizer2(BaseEstimator, VectorizerMixin):
                 'obj_score': obj_score}
 
     def _count_pos(self, text):
+        """
+        Calculate proportion of each part of speech (POS) of the text.
+        Returns
+        ----------
+            dictionary
+                keys: POS-type of word (string)
+                values: proportion (float)
+        """
         tokens = self.build_tokenizer()(text)
         text = nltk.Text(tokens)
         tagged = nltk.pos_tag(text, tagset='universal')
@@ -78,7 +108,7 @@ class Vectorizer2(BaseEstimator, VectorizerMixin):
         return res
 
     def fit(self, raw_documents, y=None):
-        """Learn a vocabulary dictionary of all tokens in the raw documents.
+        """Learn a sentiment vocabulary dictionary of all tokens in the raw documents.
         Parameters
         ----------
         raw_documents : iterable
@@ -91,7 +121,7 @@ class Vectorizer2(BaseEstimator, VectorizerMixin):
         return self
 
     def fit_transform(self, raw_documents, y=None):
-        """Learn the vocabulary dictionary and return features matrix.
+        """Learn the sentiment vocabulary dictionary and return features matrix.
         This is equivalent to fit followed by transform.
         Parameters
         ----------
@@ -99,36 +129,89 @@ class Vectorizer2(BaseEstimator, VectorizerMixin):
             An iterable which yields either str, unicode or file objects.
         Returns
         -------
-        matrix : array, [n_samples, n_features]
+        matrix : csc sparse matrix array, [n_samples, n_features]
             Document-features matrix.
         """
 
-        matrix = np.zeros((len(raw_documents), len(self.part_of_speech)+3))
         cont = 0
+        rows = []
+        cols = []
+        data = []
         for doc in raw_documents:
             doc = self._remove_stop_words(self.preprocessor(doc))
-            print(doc)
 
             sentiment = self._get_avg_sentiment(doc)
             pos = self._count_pos(doc)
-            matrix[cont, 0] = sentiment['pos_score']
-            matrix[cont, 1] = sentiment['neg_score']
-            matrix[cont, 2] = sentiment['obj_score']
-            matrix[cont, 3] = pos['ADJ']
-            matrix[cont, 4] = pos['ADP']
-            matrix[cont, 5] = pos['ADV']
-            matrix[cont, 6] = pos['CONJ']
-            matrix[cont, 7] = pos['DET']
-            matrix[cont, 8] = pos['NOUN']
-            matrix[cont, 9] = pos['NUM']
-            matrix[cont, 10] = pos['PRT']
-            matrix[cont, 11] = pos['PRON']
-            matrix[cont, 12] = pos['VERB']
-            matrix[cont, 13] = pos['.']
-            matrix[cont, 14] = pos['X']
+
+            if sentiment['pos_score']!=0:
+                rows.append(cont)
+                cols.append(0)
+                data.append(sentiment['pos_score'])
+            if sentiment['neg_score']!=0:
+                rows.append(cont)
+                cols.append(1)
+                data.append(sentiment['neg_score'])
+            if sentiment['obj_score']!=0:
+                rows.append(cont)
+                cols.append(2)
+                data.append(sentiment['obj_score'])
+            if pos['ADJ']!=0:
+                rows.append(cont)
+                cols.append(3)
+                data.append(pos['ADJ'])
+            if pos['ADP']!=0:
+                rows.append(cont)
+                cols.append(4)
+                data.append(pos['ADP'])
+            if pos['ADV']!=0:
+                rows.append(cont)
+                cols.append(5)
+                data.append(pos['ADV'])
+            if pos['CONJ']!=0:
+                rows.append(cont)
+                cols.append(6)
+                data.append(pos['CONJ'])
+            if pos['DET']!=0:
+                rows.append(cont)
+                cols.append(7)
+                data.append(pos['DET'])
+            if pos['NOUN']!=0:
+                rows.append(cont)
+                cols.append(8)
+                data.append(pos['NOUN'])
+            if pos['NUM']!=0:
+                rows.append(cont)
+                cols.append(9)
+                data.append(pos['NUM'])
+            if pos['PRT']!=0:
+                rows.append(cont)
+                cols.append(10)
+                data.append(pos['PRT'])
+            if pos['PRON']!=0:
+                rows.append(cont)
+                cols.append(11)
+                data.append(pos['PRON'])
+            if pos['VERB']!=0:
+                rows.append(cont)
+                cols.append(12)
+                data.append(pos['VERB'])
+            if pos['.']!=0:
+                rows.append(cont)
+                cols.append(13)
+                data.append(pos['.'])
+            if pos['X']!=0:
+                rows.append(cont)
+                cols.append(14)
+                data.append(pos['X'])
+
             cont += 1
 
-        self.matrix = matrix
+        rows = np.array(rows)
+        cols = np.array(cols)
+        data = np.array(data)
+        shape = (len(raw_documents), len(self.part_of_speech)+3)
+
+        self.matrix = csc_matrix((data, (rows, cols)), shape=shape)
         return self.matrix
 
     def transform(self, raw_documents):
